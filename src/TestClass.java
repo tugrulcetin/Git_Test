a3eb13c2077b65fe46e1205e151a4c9f573c4e36
tugrulcetin <av.tugrulcetin@gmail.com>
2021-01-02 22:52:47 +0100

    2 ocak

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git'den merhaba");
         System.out.println("GitHub'dan merhaba");
+        System.out.println("Bugun de boyle gecti");
     }
 }
