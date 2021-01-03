36bc041d930557ccdb63b19acf89886b4649e5c3
tugrulcetin <av.tugrulcetin@gmail.com>
2021-01-03 23:23:53 +0100

    HappyNewYear

---
+++ src/NewYear.java
@@ -0,0 +1,18 @@
+public class NewYear {
+    public static void main(String[] args) {
+        System.out.println("2021 is starting !");
+        int day = 1;
+
+        while(day < 365) {
+            System.out.println("Love");
+            System.out.println("Happiness");
+            System.out.println("peace");
+            System.out.println("Prosperity");
+            day++;
+    }
+        System.out.println("Happy New Year !");
+
+    }
+
+
+}
