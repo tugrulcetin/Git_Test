50563c34dd939d9b00f4f31347f2ef9e9e4a0f76
tugrulcetin <av.tugrulcetin@gmail.com>
2021-01-05 23:49:08 +0100

    m

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,5 +4,6 @@ public class TestClass {
         System.out.println("GitHub'dan merhaba");
         System.out.println("Bugun de boyle gecti");
         System.out.println("Bos gecen bir gun daha");
+        System.out.println("Bugun cok yurudum bakanliga ve cns e gittim 5 subat");
     }
 }
