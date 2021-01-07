954950b57c435d40fcc2f9be0babb1b8c96881f0
tugrulcetin <av.tugrulcetin@gmail.com>
2021-01-07 21:42:22 +0100

    7 ocak

--- src/TestClass.java
+++ src/TestClass.java
@@ -4,7 +4,9 @@ public class TestClass {
         System.out.println("GitHub'dan merhaba");
         System.out.println("Bugun de boyle gecti");
         System.out.println("Bos gecen bir gun daha");
-        System.out.println("Bugun cok yurudum bakanliga ve cns e gittim 5 subat");
-        System.out.println("Bugun hic yuruyus ya da spor yapamadim gunun nasil gectigini de anlamadim gece ibadet ettim ama sabahtan ogleye kadar da uyudum sonra da gece uykum gelmiyor hayatimin paradoksunu yasiyorum");
+        System.out.println("Bugun cok yurudum bakanliga ve cns e gittim 5 ocak");
+        System.out.println("6 ocak. Bugun hic yuruyus ya da spor yapamadim gunun nasil gectigini de anlamadim gece ibadet ettim ama sabahtan ogleye kadar da uyudum sonra da gece uykum gelmiyor hayatimin paradoksunu yasiyorum");
+        System.out.println("7 ocak. Bugun bisiklet surdum ve aksam da ayrica spor yaptim ama yemegi ve bulasiklari merve halletti. Ev isleri gercekten zaman mi aliyor yoksa planlarin ortasina mi denk gelliyor bilemiyorum");
+
     }
 }
