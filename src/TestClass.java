58c14cf2eee0b00568b42e3713614c544afe852c
tugrulcetin <av.tugrulcetin@gmail.com>
2021-01-09 23:13:21 +0100

    9 ocak

--- src/TestClass.java
+++ src/TestClass.java
@@ -8,5 +8,6 @@ public class TestClass {
         System.out.println("6 ocak. Bugun hic yuruyus ya da spor yapamadim gunun nasil gectigini de anlamadim gece ibadet ettim ama sabahtan ogleye kadar da uyudum sonra da gece uykum gelmiyor hayatimin paradoksunu yasiyorum");
         System.out.println("7 ocak. Bugun bisiklet surdum ve aksam da ayrica spor yaptim ama yemegi ve bulasiklari merve halletti. Ev isleri gercekten zaman mi aliyor yoksa planlarin ortasina mi denk gelliyor bilemiyorum");
         System.out.println("8 ocak. Bugun erken kalkamadim. echternah pazarina gittim. arkadaslarinin grup toplantilarina katildim");
+        System.out.println("9 ocak bugun dus, kahvalti kamyon alisverisi, 1 saatten fazla yuruyus, sut cikolata, ingilizce dersi, istisare, sohbet, duolingo ingilizce fransizca, intelij, egzersiz ile gecti");
     }
 }
